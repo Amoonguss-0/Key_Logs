7cb8e4c6265626b0627309c08d06cacdaf742b4b
Justin-F5972 <justinfernandez27@outlook.com>
2022-05-29 19:04:55 -0700

    The memberID is now considered a foreign key

---
+++ KeyLogger2/Migrations/20220530001801_updateTables.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KeyLogger2.Migrations
+{
+    public partial class updateTables : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "MemberId",
+                table: "Password",
+                newName: "MembersMemberId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Password_MembersMemberId",
+                table: "Password",
+                column: "MembersMemberId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Password_Member_MembersMemberId",
+                table: "Password",
+                column: "MembersMemberId",
+                principalTable: "Member",
+                principalColumn: "MemberId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Password_Member_MembersMemberId",
+                table: "Password");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Password_MembersMemberId",
+                table: "Password");
+
+            migrationBuilder.RenameColumn(
+                name: "MembersMemberId",
+                table: "Password",
+                newName: "MemberId");
+        }
+    }
+}
---
+++ KeyLogger2/Migrations/20220530011547_keyloggerupdate.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KeyLogger2.Migrations
+{
+    public partial class keyloggerupdate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Password_Member_MembersMemberId",
+                table: "Password");
+
+            migrationBuilder.RenameColumn(
+                name: "MembersMemberId",
+                table: "Password",
+                newName: "MemberId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Password_MembersMemberId",
+                table: "Password",
+                newName: "IX_Password_MemberId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Password_Member_MemberId",
+                table: "Password",
+                column: "MemberId",
+                principalTable: "Member",
+                principalColumn: "MemberId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Password_Member_MemberId",
+                table: "Password");
+
+            migrationBuilder.RenameColumn(
+                name: "MemberId",
+                table: "Password",
+                newName: "MembersMemberId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Password_MemberId",
+                table: "Password",
+                newName: "IX_Password_MembersMemberId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Password_Member_MembersMemberId",
+                table: "Password",
+                column: "MembersMemberId",
+                principalTable: "Member",
+                principalColumn: "MemberId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
